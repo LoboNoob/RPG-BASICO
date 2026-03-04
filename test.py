be7e1098d47ac4450cb9d6779a09cca3a6b53027
LoboNoob <rochageovane05@gmail.com>
2026-03-04 19:28:35 -0300

    Refactor RPG game menu and input handling

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
 import random
+
 clas = ["Gojo","Sukuna","Zenin","Yuji","Nenhum"]
 inatas = ["Ilimitado","Santuario","10 sombras","Restrição Celestial","Manipulação De espiritos"]
+
 print('---------------RPG BASICO---------------')
 print('Quer jogar?')
 print('Sim ou Não?')
-opcao = str(input())
+opcao = input()
 
-if opcao =='Não' or opcao == 'nao':
+if opcao.lower() in ['não', 'nao']:
     print('ok até a proxima...')
-    exit()
 
 while True:
-    print('======Menu======')
+    print('\n======Menu======')
     print('1- Iniciar Jogo')
     print('2- Customizar Personagem')
     print('3- Sair')
@@ -19,26 +20,34 @@ while True:
     opcoes = input('Escolha uma opção: ')
 
     if opcoes == '1':
-        print('Iniciando')
-    elif opcoes =='2':
+        print('Iniciando...')
+
+    elif opcoes == '2':
         print('Entrando na Customização...')
-        print("-------MENU DE CUSTOMIZAÇÃO--------")
-        print("1- Roletar Clã")
-        print("2- Roletar Inata")
-        print("3- Voltar pro Menu")
-    opc = input("Escolha uma opção: ")
-    if opc == "1":
-            print("Seu Clã é: ",random.choice(clas))
-    elif opc == "2":
-            print("Sua inata é: ",random.choice(inatas))
-    elif opc == "3":
-            print("Retornando...")
-    continue
-    
 
-    #elif opcoes =='3':
-    #    print('ok até a proxima...')
+        while True:
+            print("\n-------MENU DE CUSTOMIZAÇÃO--------")
+            print("1- Roletar Clã")
+            print("2- Roletar Inata")
+            print("3- Voltar pro Menu")
 
-    #else:
-    #    print("Opção inválida.")
-    #break
+            opc = input("Escolha uma opção: ")
+
+            if opc == "1":
+                print("Seu Clã é:", random.choice(clas))
+
+            elif opc == "2":
+                print("Sua inata é:", random.choice(inatas))
+
+            elif opc == "3":
+                print("Retornando ao menu principal...")
+                break 
+
+            else:
+                print("Opção inválida!")
+
+    elif opcoes == '3':
+        print("Saindo do jogo...")  
+
+    else:
+        print("Opção inválida!")
