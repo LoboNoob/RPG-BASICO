45587c9675427c8d8e5a9eb53388d23f8db39632
LoboNoob <rochageovane05@gmail.com>
2026-03-11 10:27:48 -0300

    Fix input handling and option comparisons in test.py

--- test.py
+++ test.py
@@ -10,19 +10,20 @@ opcao = input()
 
 if opcao.lower() in ['não', 'nao']:
     print('ok até a proxima...')
-
+    exit()
+    
 while True:
     print('\n======Menu======')
     print('1- Iniciar Jogo')
     print('2- Customizar Personagem')
     print('3- Sair')
 
-    opcoes = input('Escolha uma opção: ')
+    opcoes = int(input('Escolha uma opção: '))
 
-    if opcoes == '1':
+    if opcoes == 1:
         print('Iniciando...')
-
-    elif opcoes == '2':
+        #DAQ PARA BAIXO É O MENU DE CUSTOMIZAÇÃO
+    elif opcoes == 2:
         print('Entrando na Customização...')
 
         while True:
@@ -31,23 +32,23 @@ while True:
             print("2- Roletar Inata")
             print("3- Voltar pro Menu")
 
-            opc = input("Escolha uma opção: ")
+            opc = int(input("Escolha uma opção: "))
 
-            if opc == "1":
+            if opc == 1:
                 print("Seu Clã é:", random.choice(clas))
 
-            elif opc == "2":
+            elif opc == 2:
                 print("Sua inata é:", random.choice(inatas))
 
-            elif opc == "3":
+            elif opc == 3:
                 print("Retornando ao menu principal...")
                 break 
 
             else:
                 print("Opção inválida!")
-
-    elif opcoes == '3':
+        #TERMINA AQUI O MENU
+    elif opcoes == 3:
         print("Saindo do jogo...")  
-
+        exit()
     else:
         print("Opção inválida!")
